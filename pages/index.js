839237a2893ee28ee3523887f2ff28d5896c5e2b
pmessan <p.newman.messan@gmail.com>
2022-06-21 02:27:56 +0200

    fix(pages/index): Rework `index.ts` into JS

--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,38 @@
-import Head from 'next/head';
+import AppCarousel from '../components/AppCarousel';
+import CTASection from '../components/CTASection';
+import Container from '../components/Container';
+import FAQSection from '../components/FAQSection';
+import ImportantNumbersSection from '../components/ImportantNumbersSection';
+import { Layout } from '../components/Layout';
+import SplitSection from '../components/SplitSection';
+// type Props = {
+//   currentPage?: string,
+//   global?: any
+// }
 
 export default function Home({ global }) {
   return (
-    <>
-      <Head>
-        <title>
-          {global.attributes.siteName}
-          {' '}
-          | Home
-          {' '}
-        </title>
-        <meta name="description" content="Elevate Steps Africa" />
-        <link rel="icon" href={global.favicon} />
-      </Head>
-      <main>
-        <h1>Hello World!</h1>
-      </main>
-    </>
-
+    <Layout global={global} currentPage="Home">
+      <Container>
+        <div id="app-carousel">
+          <AppCarousel />
+        </div>
+        <div id="app-career-success;  picture left, text right">
+          <SplitSection />
+        </div>
+        <div id="empty blue section">
+          <h1>Empty blue section works!</h1>
+        </div>
+        <div id="important numbers">
+          <ImportantNumbersSection />
+        </div>
+        <div id="faeq section">
+          <FAQSection />
+        </div>
+        <div id="cta section">
+          <CTASection />
+        </div>
+      </Container>
+    </Layout>
   );
 }
