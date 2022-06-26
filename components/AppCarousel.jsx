94fdf6287a4ff3ccc4ca25aacd6c33eaf3bd4c6e
pmessan <p.newman.messan@gmail.com>
2022-06-27 01:13:07 +0200

    feat(app/landing_page): Add carousel to landing section

--- components/AppCarousel.jsx
+++ components/AppCarousel.jsx
@@ -1,38 +1,52 @@
-// import { AdvancedImage } from '@cloudinary/react';
-// import Carousel from 'react-bootstrap/Carousel';
-// import { Cloudinary } from '@cloudinary/url-gen';
-// import React from 'react';
-// import { thumbnail } from '@cloudinary/url-gen/actions/resize';
-// import { v4 as uuidv4 } from 'uuid';
+import { AdvancedImage } from '@cloudinary/react';
+import Carousel from 'react-bootstrap/Carousel';
+import { Cloudinary } from '@cloudinary/url-gen';
+import Link from 'next/link';
+import React from 'react';
+import { v4 as uuidv4 } from 'uuid';
 
-export default function AppCarousel() {
-  // const projects = files.filter((project) => project.title === title);
-  //   const files = global.attributes.landingCarousel.images;
-  //   const cld = new Cloudinary({
-  //     cloud: {
-  //       cloudName: 'elevate-steps',
-  //     },
-  //     url: {
-  //       secure: true, // force https, set to false to force http
-  //     },
-  //   });
+export default function AppCarousel({ data }) {
+  const {
+    images: { data: imageData },
+    carousselText,
+  } = data;
+  const cld = new Cloudinary({
+    cloud: {
+      cloudName: 'elevate-steps',
+    },
+    url: {
+      secure: true, // force https, set to false to force http
+    },
+  });
   return (
-    <h1>AppCarousel works!</h1>
-    // <Carousel interval="2500" key={uuidv4()} className="relative">
-    //   {files.map(
-    //     (
-    //       file, // rework
-    //     ) => (
-    //       <Carousel.Item key={uuidv4()} className="shadow-xl">
-    //         <AdvancedImage
-    //           className="object-cover object-center h-full"
-    //           cldImg={cld
-    //             .image(file.public_id)
-    //             .resize(thumbnail().width(190).height(190))}
-    //         />
-    //       </Carousel.Item>
-    //     ),
-    //   )}
-    // </Carousel>
+    <Carousel interval="3500" key={uuidv4()} className="relative">
+      {imageData.map((image, index) => (
+        <Carousel.Item
+          key={uuidv4()}
+          className="shadow-xl h-[600px] lg:h-[700px] xl:h-[800px]"
+        >
+          <div className="absolute flex items-center justify-center w-screen h-full">
+            <div className="absolute container flex flex-col items-center justify-center z-10 gap-y-3 w-fit">
+              <h1 className="text-4xl md:text-5xl lg:text-7xl xl:text-8xl text-center text-white font-medium tracking-wide max-w-[250px] md:max-w-none">
+                {carousselText[index].Header}
+              </h1>
+              <p className="md:text-xl lg:text-2xl xl:text-3xl font-light text-white max-w-[250px] md:max-w-none text-center">
+                {carousselText[index].Description}
+              </p>
+              <Link href={carousselText[index].LinkRoute} passHref>
+                <a className="hover:underline hover:text-secondary-blue text-white md:text-base lg:text-lg xl:text-xl ">
+                  {carousselText[index].LinkText}
+                </a>
+              </Link>
+            </div>
+            <div className="absolute bg-primary-blue opacity-60 h-full w-full" />
+            <AdvancedImage
+              className="object-cover object-center h-full md:h-fit"
+              cldImg={cld.image(image.attributes.provider_metadata.public_id)}
+            />
+          </div>
+        </Carousel.Item>
+      ))}
+    </Carousel>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,38 +1,56 @@
 import AppCarousel from '../components/AppCarousel';
+import AppFooter from '../components/AppFooter';
+import AppNavigation from '../components/AppNavigation';
 import CTASection from '../components/CTASection';
-import Container from '../components/Container';
 import FAQSection from '../components/FAQSection';
 import ImportantNumbersSection from '../components/ImportantNumbersSection';
 import { Layout } from '../components/Layout';
 import SplitSection from '../components/SplitSection';
+import { fetchAPI } from '../lib/api';
 // type Props = {
 //   currentPage?: string,
 //   global?: any
 // }
 
-export default function Home({ global }) {
+export default function Home({ global, homepageContent }) {
   return (
     <Layout global={global} currentPage="Home">
-      <Container>
-        <div id="app-carousel">
-          <AppCarousel />
-        </div>
-        <div id="app-career-success;  picture left, text right">
-          <SplitSection />
-        </div>
-        <div id="empty blue section">
-          <h1>Empty blue section works!</h1>
-        </div>
-        <div id="important numbers">
-          <ImportantNumbersSection />
-        </div>
-        <div id="faeq section">
-          <FAQSection />
-        </div>
-        <div id="cta section">
-          <CTASection />
-        </div>
-      </Container>
+      <AppNavigation />
+      <div id="app-carousel">
+        <AppCarousel data={homepageContent} />
+      </div>
+      <div id="app-career-success;  picture left, text right">
+        <SplitSection />
+      </div>
+      <div id="empty blue section">
+        <h1>Empty blue section works!</h1>
+      </div>
+      <div id="important numbers">
+        <ImportantNumbersSection />
+      </div>
+      <div id="faeq section">
+        <FAQSection />
+      </div>
+      <div id="cta section">
+        <CTASection />
+      </div>
+      <AppFooter />
     </Layout>
   );
+}
+
+export async function getServerSideProps() {
+  // get Homepage properties
+  const homepage = await fetchAPI('/homepage', {
+    populate: {
+      content: {
+        populate: '*',
+      },
+      defaultSeo: {
+        populate: '*',
+      },
+    },
+  });
+  // TODO: prepoluate global props before homepage data
+  return { props: { homepageContent: homepage.data.attributes.content[0] } };
 }
