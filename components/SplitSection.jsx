28d481aee1a8d4d95d4cd27e9981f45f56992321
pmessan <p.newman.messan@gmail.com>
2022-07-02 15:05:25 +0200

    feat(components/split-section): Add responsiveness to component

--- components/SplitSection.jsx
+++ components/SplitSection.jsx
@@ -5,8 +5,11 @@ export default function SplitSection({ data }) {
     heading, description, linkText, linkRoute,
   } = data;
   return (
-    <div id="split-section" className="h-[42rem] grid grid-cols-2">
-      <div id="picture" className="h-full w-full bg-orange">
+    <div
+      id="split-section"
+      className="h-[35rem] lg:h-[42rem] grid md:grid-cols-2"
+    >
+      <div id="picture" className="hidden md:block h-full w-full bg-orange">
         {/* <AdvancedImage
           className="object-cover object-center h-full md:h-fit"
           cldImg={cld.image(imageData[0].attributes.provider_metadata.public_id)}
@@ -17,12 +20,13 @@ export default function SplitSection({ data }) {
         className="flex flex-col justify-center h-full w-full bg-primary-blue"
       >
         <div className="flex flex-row items-center mx-auto max-w-lg">
-          <div id="content" className="flex flex-col text-center gap-y-5">
+          <div id="content" className="flex flex-col text-center gap-y-5 px-14">
             <h1 className="text-5xl text-white">
               {heading || 'Our Objective? Your Career Success'}
             </h1>
             <p className="text-xl text-secondary-blue">
-              {description || 'We help prepare young, upcoming African professionals to succeed in their future career endeavours. '}
+              {description
+                || 'We help prepare young, upcoming African professionals to succeed in their future career endeavours. '}
             </p>
             <Link href={linkRoute || '#'} passHref>
               <a className="text-lg hover:text-secondary-blue hover:underline text-green">
