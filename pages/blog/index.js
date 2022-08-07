ce2c63cd1e80f4d011ad43b827820a91f4ed8b36
edwin.agbakpe <kagbakpe@gmail.com>
2022-08-07 13:30:20 +0200

    feat(blog): basic blog index page

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -1,18 +1,29 @@
 import { Page } from '../../components/Page';
 import { BlogIntroSection } from '../../components/BlogIntroSection';
 import { BlogListSection } from '../../components/BlogListSection';
+import { fetchAPI } from '../../lib/api';
 
-export default function BlogHome({ global }) {
+export default function BlogHome({ global, blogPosts }) {
+  const latestBlog = blogPosts
+    /* eslint-disable-next-line max-len */
+    && blogPosts.reduce((prev, curr) => (prev.attributes.createdAt < curr.attributes.createdAt ? prev : curr));
   return (
     <Page global={global} currentPage="Blog">
-      <div className="bg-primary-blue pb-24">
-        <div id="blog-intro">
-          <BlogIntroSection />
+      {blogPosts && (
+        <div className="bg-primary-blue pb-24">
+          <div id="blog-intro">
+            <BlogIntroSection latestPost={latestBlog} />
+          </div>
+          <div id="blog-list">
+            <BlogListSection posts={blogPosts} />
+          </div>
         </div>
-        <div id="blog-list">
-          <BlogListSection />
-        </div>
-      </div>
+      )}
     </Page>
   );
+}
+
+export async function getStaticProps() {
+  const blogPostsRes = await fetchAPI('/blog-posts', { populate: '*' });
+  return { props: { blogPosts: blogPostsRes.data } };
 }
