b532bc88fdadb7f191f1128ad19720695571281b
edwin.agbakpe <kagbakpe@gmail.com>
2022-08-31 23:31:58 +0200

    feat(profiles): connect to CMS + styling changes

--- components/ProfileCatalogEntry.jsx
+++ components/ProfileCatalogEntry.jsx
@@ -1,31 +1,92 @@
 import { AdvancedImage } from '@cloudinary/react';
 import { Cloudinary } from '@cloudinary/url-gen';
 
-export default function ProfileCatalogEntry({ data }) {
-    const cld = new Cloudinary({
-        cloud: {
-          cloudName: 'elevate-steps',
-        },
-        url: {
-          secure: true, // force https, set to false to force http
-        },
-      });
-    return (
-        <div className="mt-5 w-full md:w-10/12 mx-auto border-2 h-36 rounded-lg flex flex-row" id="grid item">
-            <div className="h-full w-1/4" id="profile-pic">
-                {/* <AdvancedImage
-                className="object-cover object-center h-full w-full"
-                cldImg={cld.image(image.attributes.provider_metadata.public_id)}
-            /> */}
-            </div>
-            <div className="h-full w-3/4 py-5 pl-3 md:p-5" id="Contact information">
-                <h2 className="text-xl md:text-2xl font-cursive text-primary-blue" id="name">Name of Person</h2>
-                <h3 className="text-sm" id="position">Position at some firm</h3>
-                <div id="contact-details" className="flex flex-row w-full gap-x-10 mt-6 text-sm">
-                    <a className="text-secondary-blue hover:underline" href={"mailto:"}>Email</a>
-                    <a className="text-secondary-blue hover:underline" href={"https://:"}>Social Media</a>
-                </div>
-            </div>
+export default function ProfileCatalogEntry({
+  firstName,
+  middleName,
+  lastName,
+  graduationDate = NaN,
+  socialMedia,
+  position,
+  email,
+  photo,
+}) {
+  function renderIcon(platform) {
+    switch (platform) {
+      case 'Instagram':
+        return <i className="bi bi-instagram" />;
+      case 'Twitter':
+        return <i className="bi bi-twitter" />;
+      case 'LinkedIn':
+        return <i className="bi bi-linkedin" />;
+      case 'Facebook':
+        return <i className="bi bi-facebook" />;
+      default:
+        return <i className="bi bi-bug" />;
+    }
+  }
+
+  const cld = new Cloudinary({
+    cloud: {
+      cloudName: 'elevate-steps',
+    },
+    url: {
+      secure: true, // force https, set to false to force http
+    },
+  });
+  console.log(graduationDate);
+  const fullName = middleName
+    ? `${firstName} ${middleName} ${lastName}`
+    : `${firstName} ${lastName}`;
+  return (
+    <div
+      className="mt-5 w-full md:w-10/12 mx-auto border-2 h-36 rounded-lg flex flex-row"
+      id="grid item"
+    >
+      <div className="h-full w-1/3 md:w-1/4" id="profile-pic">
+        {photo.data && (
+          <AdvancedImage
+            className="object-cover object-center rounded-l-md h-full w-full"
+            cldImg={cld.image(
+              photo.data.attributes.provider_metadata.public_id,
+            )}
+          />
+        )}
+      </div>
+      <div
+        className="h-full w-2/3 md:w-3/4 py-2 md:py-5 px-3 md:p-5 flex flex-col justify-between"
+        id="Contact information"
+      >
+        <div>
+          <h2
+            className="text-lg md:text-xl md:text-2xl font-cursive text-primary-blue"
+            id="name"
+          >
+            {fullName}
+          </h2>
+          <h3 className="text-xs" id="position">
+            {position}
+          </h3>
         </div>
-    )
+        <div
+          id="contact-details"
+          className="flex flex-wrap justify-between w-full text-xs md:text-sm"
+        >
+          <a className="text-secondary-blue hover:underline" href="mailto:">
+            {email}
+          </a>
+          <div className="space-x-2">
+            {socialMedia.map((sm) => (
+              <a
+                className="text-primary-blue hover:underline"
+                href={sm.linkRoute}
+              >
+                {renderIcon(sm.platform)}
+              </a>
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
--- components/TabsSection.jsx
+++ components/TabsSection.jsx
@@ -1,33 +1,66 @@
-import ProfileCatalogEntry from "./ProfileCatalogEntry";
+import ProfileCatalogEntry from './ProfileCatalogEntry';
 
-export default function TabsSection({ data }) {
-    return (
-        <>
-            <ul class="nav nav-tabs" id="myTab" role="tablist">
-                <li class="nav-item" role="presentation">
-                    <button class="nav-link active" id="mentors-tab" data-bs-toggle="tab" data-bs-target="#mentors-tab-pane" type="button" role="tab" aria-controls="mentors-tab-pane" aria-selected="true">Mentors</button>
-                </li>
-                <li class="nav-item" role="presentation">
-                    <button class="nav-link" id="fellows-tab" data-bs-toggle="tab" data-bs-target="#fellows-tab-pane" type="button" role="tab" aria-controls="fellows-tab-pane" aria-selected="false">Fellows</button>
-                </li>
-            </ul>
-            <div class="tab-content" id="myTabContent">
-                <div class="tab-pane fade show active" id="mentors-tab-pane" role="tabpanel" aria-labelledby="mentors-tab" tabindex="0">
-                    <div className="grid xl:grid-cols-2" id="grid for mentors">
-                        {/* for loop  */}
-                        <ProfileCatalogEntry />
-                        <ProfileCatalogEntry />
-                        <ProfileCatalogEntry />
-                    </div>
-                </div>
-                <div class="tab-pane fade" id="fellows-tab-pane" role="tabpanel" aria-labelledby="fellows-tab" tabindex="0">
-                    <div className="grid xl:grid-cols-2" id="grid for mentors">
-                        {/* for loop  */}
-                        <ProfileCatalogEntry />
-                    </div>
-                </div>
-
+export default function TabsSection({ tabs }) {
+  return (
+    <>
+      <ul className="nav nav-tabs" id="myTab" role="tablist">
+        <li className="nav-item" role="presentation">
+          <button
+            className="nav-link active"
+            id="mentors-tab"
+            data-bs-toggle="tab"
+            data-bs-target="#mentors-tab-pane"
+            type="button"
+            role="tab"
+            aria-controls="mentors-tab-pane"
+            aria-selected="true"
+          >
+            Mentors
+          </button>
+        </li>
+        <li className="nav-item" role="presentation">
+          <button
+            className="nav-link"
+            id="fellows-tab"
+            data-bs-toggle="tab"
+            data-bs-target="#fellows-tab-pane"
+            type="button"
+            role="tab"
+            aria-controls="fellows-tab-pane"
+            aria-selected="false"
+          >
+            Fellows
+          </button>
+        </li>
+      </ul>
+      <div className="tab-content" id="myTabContent">
+        {tabs.map((tab, index) => (
+          <div
+            key={tab.name}
+            className={`tab-pane fade ${index === 0 && 'show active'}`}
+            id={`${tab.name}-tab-pane`}
+            role="tabpanel"
+            aria-labelledby={`${tab.name}-tab`}
+            tabIndex={index}
+          >
+            <div className="grid xl:grid-cols-2" id={`grid-${tab.name}`}>
+              {tab.list.map((profile) => (
+                <ProfileCatalogEntry
+                  key={`${tab.name}-${profile.id}`}
+                  firstName={profile.firstName}
+                  lastName={profile.lastName}
+                  middleName={profile.middleName}
+                  graduationDate={profile.graduationDate ?? NaN}
+                  position={profile.position}
+                  email={profile.email}
+                  socialMedia={profile.socialMedia}
+                  photo={profile.photo}
+                />
+              ))}
             </div>
-        </>
-    )
+          </div>
+        ))}
+      </div>
+    </>
+  );
 }
