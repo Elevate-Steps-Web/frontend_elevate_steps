c2e824f963796a113e2d99c9fa5af7a515befd50
edwin.agbakpe <kagbakpe@gmail.com>
2022-08-31 23:31:58 +0200

    feat(profiles): connect to CMS + styling changes

--- pages/about/profiles.js
+++ pages/about/profiles.js
@@ -1,61 +1,49 @@
 import Container from '../../components/Container';
 import { Page } from '../../components/Page';
-import PageHeader from '../../components/PageHeader';
+// import PageHeader from '../../components/PageHeader';
 import TabsSection from '../../components/TabsSection';
 import { fetchAPI } from '../../lib/api';
 
-export default function ProfilesPage({
-    global,
-    // pageContent: {
-    //   pageHeader,
-    // },
-}) {
-
-    return (
-        <Page global={global} currentPage="Contact Us">
-            <Container>
-                {/* {pageHeader && <PageHeader data={pageHeader} />} */}
-                <TabsSection />
-            </Container>
-        </Page>
-    )
+export default function ProfilesPage({ global, tabs }) {
+  return (
+    <Page global={global} currentPage="Contact Us">
+      <Container>
+        {/* {pageHeader && <PageHeader data={pageHeader} />} */}
+        <TabsSection tabs={tabs} />
+      </Container>
+    </Page>
+  );
 }
 
+export async function getStaticProps({ locale: routeLocale }) {
+  // get Page properties
+  const page = await fetchAPI('/profile', {
+    populate: {
+      mentors: {
+        populate: '*',
+      },
+      fellows: {
+        populate: '*',
+      },
+    },
+    locale: routeLocale,
+  });
+  // TODO: loop through objects and filter objects based on __component property
+  const {
+    data: {
+      attributes: { mentors, fellows },
+    },
+  } = page;
+  if (mentors === null && fellows === null) {
+    console.log('No data content in contact page.');
+    return {
+      notFound: true,
+    };
+  }
+  const tabs = [
+    { name: 'mentors', list: mentors },
+    { name: 'fellows', list: fellows },
+  ];
 
-
-// export async function getStaticProps({ locale: routeLocale }) {
-//     // get Page properties
-//     const page = await fetchAPI('/about/profiles', {
-//         populate: {
-//             content: {
-//                 populate: '*',
-//             },
-//             defaultSeo: {
-//                 populate: '*',
-//             },
-//         },
-//         locale: routeLocale,
-//     });
-//     // TODO: loop through objects and filter objects based on __component property
-//     const pageContent = {};
-//     const {
-//         data: {
-//             attributes: { content },
-//         },
-//     } = page;
-//     if (content === null) {
-//         console.log('No data content in contact page.');
-//         return {
-//             notFound: true,
-//         };
-//     }
-//     content.forEach((item) => {
-//         /* eslint-disable-next-line no-underscore-dangle */
-//         switch (item.__component) {
-//             case 'text.page-header':
-//                 pageContent.pageHeader = item;
-//                 break;
-//         }
-//     });
-//     return { props: { pageContent } };
-// }
+  return { props: { tabs } };
+}
