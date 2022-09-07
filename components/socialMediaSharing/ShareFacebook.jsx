ca5282596b26333d8594ea6428ddff61bc84e73b
pmessan <p.newman.messan@gmail.com>
2022-09-07 16:15:34 +0200

    feat(blog): Add sharing buttons for Facebook and Twitter

---
+++ components/socialMediaSharing/ShareFacebook.jsx
@@ -0,0 +1,22 @@
+import { useRouter } from 'next/router';
+
+export default function ShareFacebook() {
+  const router = useRouter();
+  const { URL } = process.env;
+
+  return (
+    <iframe
+      title="Share to facebook"
+      src={`https://www.facebook.com/plugins/share_button.php?href=${encodeURI(
+        URL + router.asPath,
+      )}&layout=button&size=large&appId=393841796264916&width=77&height=28`}
+      width="77"
+      height="28"
+      style={{ border: 'none', overflow: 'hidden' }}
+      scrolling="no"
+      frameBorder="0"
+      allowFullScreen="true"
+      allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"
+    />
+  );
+}
