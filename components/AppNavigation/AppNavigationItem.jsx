2d242206f89f210a56b04ac005071eeadf04e7aa
pmessan <p.newman.messan@gmail.com>
2022-09-07 16:13:08 +0200

    fix(nav): Add some spacing to the nav

--- components/AppNavigation/AppNavigationItem.jsx
+++ components/AppNavigation/AppNavigationItem.jsx
@@ -3,7 +3,7 @@ import Link from 'next/link';
 export default function AppNavigationItem({ linkText, linkRoute }) {
   return (
     <Link href={linkRoute} passHref>
-      <a className="nav-link hover:text-secondary-blue text-white">
+      <a className="nav-link hover:text-secondary-blue text-white tracking-wide">
         {linkText}
       </a>
     </Link>
