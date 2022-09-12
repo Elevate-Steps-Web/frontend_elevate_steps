5852b33d685913f30e6ba2cef33ac3459305d09f
pmessan <p.newman.messan@gmail.com>
2022-09-12 02:23:57 +0200

    refactor(programs/mentorship): Reorganise mentorship program-related pages

---
+++ pages/about/programs/index.js
@@ -0,0 +1,47 @@
+import Link from 'next/link';
+import { v4 as uuidv4 } from 'uuid';
+import Container from '../../../components/Container';
+import { Page } from '../../../components/Page';
+import { fetchAPI } from '../../../lib/api';
+
+export default function ProgramsPage({ global, pageData }) {
+  const {
+    attributes: {
+      pageHeader: { pageTitle, caption },
+      programs,
+    },
+  } = pageData;
+  return (
+    <Page global={global} currentPage="Programs">
+      <div className="flex flex-col items-center text-center py-10">
+        <Container>
+          <h1 className="font-cursive text-6xl mb-10 text-primary-blue">
+            {pageTitle}
+          </h1>
+          <p className="text-2xl lg:px-36 md:px-24">{caption}</p>
+        </Container>
+      </div>
+      <Container>
+        <div className="w-full h-20 mx-42 px-10 md:px-28 lg:px-36">
+          {programs.map((program) => (
+            <Link key={uuidv4} href={program.linkRoute} passHref>
+              <a className="text-xl md:text-3xl lg:text-4xl text-center hover:text-primary-blue text-white tracking-wide w-full ">
+                <div className="w-full h-10 md:h-14 lg:h-16 bg-primary-blue hover:bg-secondary-blue flex flex-col justify-center rounded-xl">
+                  {program.name}
+                </div>
+              </a>
+            </Link>
+          ))}
+        </div>
+      </Container>
+    </Page>
+  );
+}
+
+export async function getServerSideProps({ locale: routeLocale }) {
+  const page = await fetchAPI('/about-our-programs', {
+    populate: '*',
+    locale: routeLocale,
+  });
+  return { props: { pageData: page.data } };
+}
