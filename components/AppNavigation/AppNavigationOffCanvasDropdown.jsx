5a3f225b109d0aadd38f3502d3c5c5707ad14135
edwin.agbakpe <kagbakpe@gmail.com>
2022-09-13 01:03:24 +0200

    feat(nav): switch to treeview component for offcanvas menu

--- components/AppNavigation/AppNavigationOffCanvasDropdown.jsx
+++ components/AppNavigation/AppNavigationOffCanvasDropdown.jsx
@@ -1,41 +1,28 @@
-import Link from 'next/link';
 import { v4 as uuidv4 } from 'uuid';
+import TreeView from '@mui/lab/TreeView';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import ChevronDownIcon from '@mui/icons-material/ChevronRight';
+import TreeItem from '@mui/lab/TreeItem';
 import AppNavigationOffCanvasDropdownItem from './AppNavigationOffCanvasDropdownItem';
 
 export default function AppNavigationOffCanvasDropdown({ title, navItems }) {
   return (
     <span className="dropdown">
-      <span
-        className="dropdown-toggle hover:text-orange  active:underline"
-        id="navbarDropdownMenuLink"
-        role="button"
-        data-bs-toggle="dropdown"
-        aria-expanded="false"
+      <TreeView
+        aria-label={`${title} dropdown menu`}
+        defaultCollapseIcon={<ExpandMoreIcon />}
+        defaultExpandIcon={<ChevronDownIcon />}
       >
-        {title}
-      </span>
-      <div
-        className="dropdown-menu bg-secondary-blue rounded-b-lg border-orange px-2"
-        aria-labelledby="navbarDropdownMenuLink"
-      >
-        {navItems.map((navItem) => (
-          <AppNavigationOffCanvasDropdownItem
-            key={uuidv4()}
-            linkText={navItem.linkText}
-            linkRoute={navItem.linkRoute}
-          />
-        ))}
-        <Link href="/about/what-we-do" passHref>
-          <a className="block hover:text-orange text-xl  active:underline">
-            What We Do
-          </a>
-        </Link>
-        <Link href="/about/partners-&-sponsors" passHref>
-          <a className="block hover:text-orange text-xl  active:underline">
-            Partners and Sponsors
-          </a>
-        </Link>
-      </div>
+        <TreeItem nodeId="1" label={title}>
+          {navItems.map((navItem) => (
+            <AppNavigationOffCanvasDropdownItem
+              key={uuidv4()}
+              linkText={navItem.linkText}
+              linkRoute={navItem.linkRoute}
+            />
+          ))}
+        </TreeItem>
+      </TreeView>
     </span>
   );
 }
