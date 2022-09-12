a1610dce27dd68a642c7867c3a6a0d1bfd06d8a9
pmessan <p.newman.messan@gmail.com>
2022-09-12 17:32:03 +0200

    feat(app/error): Add 404 and error pages

--- pages/404.js
+++ pages/404.js
@@ -1,12 +1,78 @@
-import Link from 'next/link';
+/* eslint-disable no-underscore-dangle */
 
-export default function FourOhFour() {
+import { AdvancedImage } from '@cloudinary/react';
+import { Cloudinary } from '@cloudinary/url-gen';
+import Link from 'next/link';
+import Container from '../components/Container';
+import { Page } from '../components/Page';
+
+export default function FourOhFour({ global }) {
+  const cld = new Cloudinary({
+    cloud: {
+      cloudName: 'elevate-steps',
+    },
+    url: {
+      secure: true,
+    },
+  });
+  function get404Text(routeLocale) {
+    switch (routeLocale) {
+      case 'en':
+        return {
+          caption: "Looks like you're lost!",
+          homePrompt: 'Wanna head back home? Hit the bouncing button below!',
+        };
+      case 'fr':
+        return {
+          caption: 'On dirait que tu es perdu!',
+          homePrompt:
+            'Vous voulez rentrer? Appuyez sur le bouton rebondissant ci-dessous!',
+        };
+      default:
+        return {
+          caption: "Looks like you're lost!",
+          homePrompt: 'Wanna head back home? Hit the bouncing button below!',
+        };
+    }
+  }
+  const errorText = get404Text(global.attributes.locale);
   return (
-    <>
-      <h1>404 - Page Not Found</h1>
-      <Link href="/">
-        <a>Go back home</a>
-      </Link>
-    </>
+    <Page global={global} currentPage="404">
+      <div className="flex flex-col items-center py-24">
+        <Container>
+          <div className="flex flex-col items-center text-center px-6">
+            <h1 className="text-9xl lg:text-[16rem] tracking-widest font-cursive text-orange">
+              404
+            </h1>
+            <h2 className="text-3xl lg:text-4xl font-cursive text-orange mb-24">
+              {errorText.caption}
+            </h2>
+            <h2 className="text-3xl text-primary-blue">
+              {errorText.homePrompt}
+            </h2>
+            <Link href="/" passHref>
+              <a className="h-36 lg:h-56 w-fit">
+                <div className="h-18 md:h-32 mt-24 bounce-down">
+                  {global.attributes.footer
+                    .filter(
+                      (navItem) => navItem.__component === 'images.nav-brand',
+                    )
+                    .map((image) => (
+                      <AdvancedImage
+                        key={1}
+                        className="object-cover object-center h-full lg:h-16 w-fit"
+                        cldImg={cld.image(
+                          image.navBrand.data.attributes.provider_metadata
+                            .public_id,
+                        )}
+                      />
+                    ))}
+                </div>
+              </a>
+            </Link>
+          </div>
+        </Container>
+      </div>
+    </Page>
   );
 }
--- pages/_error.js
+++ pages/_error.js
@@ -1,10 +1,65 @@
-function Error({ statusCode }) {
+/* eslint-disable no-underscore-dangle */
+
+import { AdvancedImage } from '@cloudinary/react';
+import { Cloudinary } from '@cloudinary/url-gen';
+import Link from 'next/link';
+import Container from '../components/Container';
+import { Layout } from '../components/Layout';
+
+function Error({ statusCode, global }) {
+  const {
+    attributes: { favicon, siteName, footer },
+  } = global;
+  const cld = new Cloudinary({
+    cloud: {
+      cloudName: 'elevate-steps',
+    },
+    url: {
+      secure: true,
+    },
+  });
   return (
-    <p>
-      {statusCode
-        ? `An error ${statusCode} occurred on server`
-        : 'An error occurred on client'}
-    </p>
+    <Layout siteName={siteName} favicon={favicon} currentPage="Error">
+      <div className="flex flex-col items-center justify-center h-screen w-full flex-grow bg-primary-blue">
+        <Container>
+          <div className="flex flex-col items-center text-center px-6">
+            <h1 className="text-white text-4xl text-center px-4 md:px-24">
+              {statusCode ? (
+                <span>{`An error ${statusCode} occurred on server while processing your request.`}</span>
+              ) : (
+                <span>
+                  An error occurred in your browser while loading this page.
+                </span>
+              )}
+              <span className="block mt-5">
+                But not to worry. Click on the logo below and head on back to
+                the homepage!
+              </span>
+            </h1>
+            <Link href="/" passHref>
+              <a className="h-36 lg:h-56 w-fit">
+                <div className="h-18 md:h-32 mt-24 bounce-down">
+                  {footer
+                    .filter(
+                      (navItem) => navItem.__component === 'images.nav-brand',
+                    )
+                    .map((image) => (
+                      <AdvancedImage
+                        key={1}
+                        className="object-cover object-center h-full lg:h-32 w-fit"
+                        cldImg={cld.image(
+                          image.navBrand.data.attributes.provider_metadata
+                            .public_id,
+                        )}
+                      />
+                    ))}
+                </div>
+              </a>
+            </Link>
+          </div>
+        </Container>
+      </div>
+    </Layout>
   );
 }
 
