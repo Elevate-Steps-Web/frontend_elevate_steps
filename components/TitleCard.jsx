a63014fbeaa7da6caffccdc3a20c0ff187f037a9
pmessan <p.newman.messan@gmail.com>
2022-09-21 01:03:07 +0200

    feat(/about/mentorship page): Redesign buttons

---
+++ components/TitleCard.jsx
@@ -0,0 +1,52 @@
+import { AdvancedImage, AdvancedVideo } from '@cloudinary/react';
+
+import { Cloudinary } from '@cloudinary/url-gen';
+
+export default function TitleCard({
+  title,
+  bg: {
+    data: {
+      attributes: {
+        formats: {
+          medium: {
+            provider_metadata: { public_id: mediaId, resource_type: mediaType },
+          },
+        },
+      },
+    },
+  },
+}) {
+  const cld = new Cloudinary({
+    cloud: {
+      cloudName: 'elevate-steps',
+    },
+    url: {
+      secure: true,
+    },
+  });
+  return (
+    <div className="flex flex-col justify-center h-72 w-[25rem] border-2 border-grey-500 hover:border-green rounded-lg transition duration-200 ease-in-out hover:scale-110">
+      <div className="absolute flex flex-row z-20 h-72 w-[25rem]">
+        <div className="flex flex-col items-center w-full h-full justify-center">
+          <h2 className="text-3xl md:text-4xl xl:text-5xl text-center text-white">
+            {title}
+          </h2>
+        </div>
+      </div>
+      <div className="absolute bg-primary-blue z-0 opacity-30 h-[17.8rem] w-[24.8rem] rounded-md" />
+      {mediaType === 'image' && (
+        <AdvancedImage
+          className="object-cover object-center h-full w-full rounded-md"
+          cldImg={cld.image(mediaId)}
+        />
+      )}
+      {mediaType === 'video' && (
+        <AdvancedVideo
+          className="object-cover object-center h-full w-full py-14"
+          cldVid={cld.video(mediaId)}
+          autoPlay
+        />
+      )}
+    </div>
+  );
+}
--- pages/about/programs/index.js
+++ pages/about/programs/index.js
@@ -6,6 +6,7 @@ import { v4 as uuidv4 } from 'uuid';
 import Container from '../../../components/Container';
 import Loading from '../../../components/Loading';
 import { Page } from '../../../components/Page';
+import TitleCard from '../../../components/TitleCard';
 import { fetchAPI } from '../../../lib/api';
 
 export default function ProgramsPage({ global, pageData, notFound }) {
@@ -33,13 +34,11 @@ export default function ProgramsPage({ global, pageData, notFound }) {
         </Container>
       </div>
       <Container>
-        <div className="w-full h-20 mx-42 px-10 md:px-28 lg:px-36">
+        <div className="flex  flex-wrap w-full mx-42 px-10 md:px-20 lg:px-0 justify-center justify-items-between gap-y-12 gap-x-12 mb-36">
           {programs.map((program) => (
-            <Link key={uuidv4} href={program.linkRoute} passHref>
-              <a className="text-xl md:text-3xl lg:text-4xl text-center hover:text-primary-blue text-white tracking-wide w-full ">
-                <div className="w-full h-10 md:h-14 lg:h-16 bg-primary-blue hover:bg-secondary-blue flex flex-col justify-center rounded-xl">
-                  {program.name}
-                </div>
+            <Link key={uuidv4()} href={program.linkRoute} passHref>
+              <a>
+                <TitleCard title={program.name} bg={program.bgImage} />
               </a>
             </Link>
           ))}
@@ -51,7 +50,17 @@ export default function ProgramsPage({ global, pageData, notFound }) {
 
 export async function getServerSideProps({ locale: routeLocale }) {
   const page = await fetchAPI('/about-our-programs', {
-    populate: '*',
+    populate: {
+      pageHeader: {
+        populate: '*',
+      },
+      programs: {
+        populate: '*',
+      },
+      defaultSeo: {
+        populate: '*',
+      },
+    },
     locale: routeLocale,
   });
   if (page.error) {
