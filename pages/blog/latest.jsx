58c9a8ba5053d6c99de10c1f5ae5f3e730a0b1c8
pmessan <p.newman.messan@gmail.com>
2022-09-26 21:19:31 +0200

    feat(blog): Add latest page redirection

---
+++ pages/blog/latest.jsx
@@ -0,0 +1,35 @@
+import _ from 'lodash';
+import { useEffect } from 'react';
+import Loading from '../../components/Loading';
+import { fetchAPI } from '../../lib/api';
+
+export default function LatestBlogPost({ blogPosts }) {
+  const latestPost = blogPosts
+    /* eslint-disable-next-line max-len */
+    && blogPosts.reduce((prev, curr) => (Date.parse(prev.attributes.createdAt)
+      > Date.parse(curr.attributes.createdAt)
+      ? prev
+      : curr));
+  const {
+    id,
+    attributes: { blogTitle },
+  } = latestPost;
+  useEffect(() => {
+    window.location.href = `/blog/${id}/${_.kebabCase(blogTitle)}`;
+  });
+  return <Loading state />;
+}
+
+export async function getServerSideProps({ locale: routeLocale }) {
+  const blogPostsRes = await fetchAPI('/blog-posts', {
+    populate: '*',
+    locale: routeLocale,
+  });
+  if (blogPostsRes.error) {
+    console.log('Fetching data failed');
+    return {
+      notFound: true,
+    };
+  }
+  return { props: { blogPosts: blogPostsRes.data } };
+}
